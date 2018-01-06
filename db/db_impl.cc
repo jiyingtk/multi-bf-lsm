71f8f6069824501278d6094ef9535ac1ace1d6e8
mingfire@mail.ustc.edu.cn <mingfire>
2018-01-06 17:27:11 +0800

    full filter and only count on zero-result lookup in statistics

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -1182,9 +1182,11 @@ Status DBImpl::Get(const ReadOptions& options,
       // Done
     } else {
       s = current->Get(options, lkey, value, &stats);
-      p->setHistType(options.read_file_nums+READ_0_TIME);
+      if(s.IsNotFound()){
+	p->setHistType(options.read_file_nums+READ_0_TIME);
+	alloc_stop_watch.destroy(p);
+      }
       have_stat_update = true;
-      alloc_stop_watch.destroy(p);
     }
     alloc_stop_watch.deallocate(p,1);
     mutex_.Lock();
